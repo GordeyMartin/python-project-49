d647e5c657afac5cca8514f3cf0bdef31db6edb6
Gordey Danilochkin <gordanshow@gmail.com>
2023-09-22 23:10:26 -0500

    Step three

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+import sys
+sys.path.append("python-project-49/brain_games")
+sys.path.append("brain_games")
+
+from cli import welcome_user
+
 def main():
-	print('Welcome to the Brain Games!')
+	welcome_user()
 
 if __name__ == '__main__':
 	main()
